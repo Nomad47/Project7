493acf0d5e4c24729eff0bc809c3aae3a039dfa7
19AF OSS <mark.thrower.2@us.af.mil>
2019-08-27 15:10:56 -0500

    Initial Commit

---
+++ Project7/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Project7
+//
+//  Created by Mark on 8/27/19.
+//  Copyright © 2019 19AF OSS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
